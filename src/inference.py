75d5982b2b9d5e259cbb268b5333ca679ab7873c
KishoreK47 <kishore7487k@gmail.com>
2025-12-19 22:46:59 +0530

    Fix model path for cloud deployment

--- src/inference.py
+++ src/inference.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import joblib
+import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 MODEL_PATH = os.path.join(BASE_DIR, "models", "completion_model.joblib")
